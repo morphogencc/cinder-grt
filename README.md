03a0b368f99f53947ee74b9fad4eb733b1929134
nathan lachenmyer <nathan@midnightcommercial.com>
2017-01-30 18:25:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# midnight-grt
+
+A wrapper and packaged examples of using the Gesture Recognition Toolkit (GRT) with Cinder in Windows.
+
+## Requirements
+
+## Contributing
+This project uses the [Git Flow](http://nvie.com/posts/a-successful-git-branching-model/) paradigm.  Before contributing, please make your own feature branch with your changes.
+
+## More Information
+* [GRT Homepage](http://www.github.com/nickgillian/grt/)
---
+++ exampleClassification/src/exampleClassificationApp.cpp
@@ -0,0 +1,243 @@
+#include "cinder/app/App.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "GRT.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class exampleClassificationApp : public App {
+  public:
+	enum ClassifierType { ADABOOST = 0, 
+							DECISION_TREE, 
+							KKN, 
+							GAUSSIAN_MIXTURE_MODEL, 
+							NAIVE_BAYES, 
+							MINDIST, 
+							RANDOM_FOREST_10, 
+							RANDOM_FOREST_100, 
+							RANDOM_FOREST_200, 
+							SOFTMAX, 
+							SVM_LINEAR, 
+							SVM_RBF, 
+							NUM_CLASSIFIERS
+	};
+
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void keyDown(KeyEvent event) override;
+	void update() override;
+	void draw() override;
+	bool setClassifier(const int type);
+	std::string classifierTypeToString(const int type);
+
+	int mClassifierType;
+	GRT::ClassificationData mTrainingData;
+	GRT::GestureRecognitionPipeline mPipeline;
+	GRT::Vector<ci::Color> mClassColors;
+	UINT mTrainingClassLabel;
+};
+
+
+bool exampleClassificationApp::setClassifier(const int type) {
+
+	GRT::AdaBoost adaboost;
+	GRT::DecisionTree dtree;
+	GRT::KNN knn;
+	GRT::GMM gmm;
+	GRT::ANBC naiveBayes;
+	GRT::MinDist minDist;
+	GRT::RandomForests randomForest;
+	GRT::Softmax softmax;
+	GRT::SVM svm;
+	bool enableNullRejection = false;
+
+	mClassifierType = type;
+	mPipeline.clear();
+
+	switch (mClassifierType) {
+	case ADABOOST:
+		adaboost.enableNullRejection(enableNullRejection);
+		adaboost.setNullRejectionCoeff(3);
+		mPipeline << adaboost;
+		break;
+	case DECISION_TREE:
+		dtree.enableNullRejection(enableNullRejection);
+		dtree.setNullRejectionCoeff(3);
+		dtree.setMaxDepth(10);
+		dtree.setMinNumSamplesPerNode(3);
+		dtree.setRemoveFeaturesAtEachSplit(false);
+		mPipeline << dtree;
+		break;
+	case KKN:
+		knn.enableNullRejection(enableNullRejection);
+		knn.setNullRejectionCoeff(3);
+		mPipeline << knn;
+		break;
+	case GAUSSIAN_MIXTURE_MODEL:
+		gmm.enableNullRejection(enableNullRejection);
+		gmm.setNullRejectionCoeff(3);
+		mPipeline << gmm;
+		break;
+	case NAIVE_BAYES:
+		naiveBayes.enableNullRejection(enableNullRejection);
+		naiveBayes.setNullRejectionCoeff(3);
+		mPipeline << naiveBayes;
+		break;
+	case MINDIST:
+		minDist.enableNullRejection(enableNullRejection);
+		minDist.setNullRejectionCoeff(3);
+		mPipeline << GRT::MinDist(false, true);
+		break;
+	case RANDOM_FOREST_10:
+		randomForest.enableNullRejection(enableNullRejection);
+		randomForest.setNullRejectionCoeff(3);
+		randomForest.setForestSize(10);
+		randomForest.setNumRandomSplits(2);
+		randomForest.setMaxDepth(10);
+		randomForest.setMinNumSamplesPerNode(5);
+		randomForest.setRemoveFeaturesAtEachSplit(false);
+		mPipeline << randomForest;
+		break;
+	case RANDOM_FOREST_100:
+		randomForest.enableNullRejection(enableNullRejection);
+		randomForest.setNullRejectionCoeff(3);
+		randomForest.setForestSize(100);
+		randomForest.setNumRandomSplits(2);
+		randomForest.setMaxDepth(10);
+		randomForest.setMinNumSamplesPerNode(3);
+		randomForest.setRemoveFeaturesAtEachSplit(false);
+		mPipeline << randomForest;
+		break;
+	case RANDOM_FOREST_200:
+		randomForest.enableNullRejection(enableNullRejection);
+		randomForest.setNullRejectionCoeff(3);
+		randomForest.setForestSize(200);
+		randomForest.setNumRandomSplits(2);
+		randomForest.setMaxDepth(10);
+		randomForest.setMinNumSamplesPerNode(3);
+		randomForest.setRemoveFeaturesAtEachSplit(false);
+		mPipeline << randomForest;
+		break;
+	case SOFTMAX:
+		softmax.enableNullRejection(enableNullRejection);
+		softmax.setNullRejectionCoeff(3);
+		mPipeline << softmax;
+		break;
+	case SVM_LINEAR:
+		svm.enableNullRejection(enableNullRejection);
+		svm.setNullRejectionCoeff(3);
+		mPipeline << GRT::SVM(GRT::SVM::LINEAR_KERNEL);
+		break;
+	case SVM_RBF:
+		svm.enableNullRejection(enableNullRejection);
+		svm.setNullRejectionCoeff(3);
+		mPipeline << GRT::SVM(GRT::SVM::RBF_KERNEL);
+		break;
+	default:
+		return false;
+		break;
+	}
+
+	return true;
+}
+
+std::string exampleClassificationApp::classifierTypeToString(const int type) {
+	switch (type) {
+	case ADABOOST:
+		return "ADABOOST";
+		break;
+	case DECISION_TREE:
+		return "DECISION_TREE";
+		break;
+	case KKN:
+		return "KKN";
+		break;
+	case GAUSSIAN_MIXTURE_MODEL:
+		return "GMM";
+		break;
+	case NAIVE_BAYES:
+		return "NAIVE_BAYES";
+		break;
+	case MINDIST:
+		return "MINDIST";
+		break;
+	case RANDOM_FOREST_10:
+		return "RANDOM_FOREST_10";
+		break;
+	case RANDOM_FOREST_100:
+		return "RANDOM_FOREST_100";
+		break;
+	case RANDOM_FOREST_200:
+		return "RANDOM_FOREST_200";
+		break;
+	case SOFTMAX:
+		return "SOFTMAX";
+		break;
+	case SVM_LINEAR:
+		return "SVM_LINEAR";
+		break;
+	case SVM_RBF:
+		return "SVM_RBF";
+		break;
+	}
+	return "UNKOWN_CLASSIFIER";
+}
+
+void exampleClassificationApp::setup() {
+	mTrainingClassLabel = 1;
+	mTrainingData.setNumDimensions(2);
+	setClassifier(NAIVE_BAYES);
+
+	mClassColors.resize(3);
+	mClassColors[0] = ci::Color(255, 0, 0);
+	mClassColors[1] = ci::Color(0, 255, 0);
+	mClassColors[2] = ci::Color(0, 0, 255);
+}
+
+void exampleClassificationApp::mouseDown( MouseEvent event ) {
+	//Grab the current mouse x and y position
+	GRT::VectorFloat sample(2);
+	sample[0] = event.getPos().x / double(ci::app::getWindowWidth());
+	sample[1] = event.getPos().y / double(ci::app::getWindowHeight());
+
+	mTrainingData.addSample(mTrainingClassLabel, sample);
+}
+
+void exampleClassificationApp::keyDown(KeyEvent event) {
+	if (event.getChar() == '1') {
+		mTrainingClassLabel = 1;
+	}
+	else if (event.getChar() == '2') {
+		mTrainingClassLabel = 2;
+	}
+	else if (event.getChar() == '3') {
+		mTrainingClassLabel = 3;
+	}
+}
+
+
+void exampleClassificationApp::update() {
+	//If the pipeline has been trained, then run the prediction
+	/*
+	if (mPipeline.getTrained()) {
+		mPipeline.predict(sample);
+	}
+	*/
+}
+
+void exampleClassificationApp::draw() {
+	gl::clear( Color( 0, 0, 0 ) );
+	for (unsigned int i = 0; i < mTrainingData.getNumSamples(); i++) {
+		float x = mTrainingData[i][0] * ci::app::getWindowWidth();
+		float y = mTrainingData[i][1] * ci::app::getWindowHeight();
+
+		gl::color(Color(0, 0, 0));
+		ci::gl::drawSolidCircle(ci::vec2(x, y), 6);
+		gl::color(mClassColors[mTrainingData[i].getClassLabel() - 1 % mClassColors.size()]);
+		ci::gl::drawSolidCircle(ci::vec2(x, y), 5);
+	}
+}
+
+CINDER_APP( exampleClassificationApp, RendererGl )
